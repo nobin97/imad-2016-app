7028f6d2c2d10a8c40de4262edca2d78dcc93cae
nobin97 <nobin97@gmail.com>
2017-08-31 13:16:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+   res.send('<html><body>Logged out Successfully!<br/><br/><a href="/">Back to homepage</a></body></html>');
 });
 
 var pool = new Pool(config);
